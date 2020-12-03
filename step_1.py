02050520c8d38e5415919f0145ecd702504f1c1b
Никита <mr.gonk@mail.ru>
2020-12-03 12:03:45 +0500

    initial

--- step_1.py
+++ step_1.py
@@ -1,6 +1,30 @@
 candy = ['Шоколадная', 'Каромельная', 'Мармеладная', 'Смешаная', 'Трюфельная']
-print(candy)
+print('Шоколадная - 1, Каромельная - 2, Мармеладная - 3, Смашаня - 4, Трюфельная - 5')
 hp = input('Если какая-то конфета не нравится напиши её номер, начиная с 0:')
-if hp == '0':
-    fg = list.pop(0)
-    print(fg)
+if hp == '1':
+    candy.pop(0)
+    print(candy)
+elif hp == '2':
+    candy.pop(1)
+    print(candy)
+elif hp == '3':
+    candy.pop(2)
+    print(candy)
+elif hp == '4':
+    candy.pop(3)
+    print(candy)
+elif hp == '5':
+    candy.pop(4)
+    print(candy)
+else:
+    print('Такого нет в спске!')
+gh = input('Желаете увеличить список?')
+if gh == 'Нет':
+    print('Наслаждайтесь тем что есть!')
+elif gh == 'нет':
+    print('Наслаждайтесь тем что есть!')
+elif gh == 'Да':
+    print(input('Какую кофету вы хотите добавить?'))
+elif gh == 'да':
+    print(input('Какую кофету вы хотите добавить?'))
+print(dir(candy))
